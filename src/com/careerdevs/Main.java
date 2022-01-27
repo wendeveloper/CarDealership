2296134a4fef7b47785c985f3d25d4c6018b5881
Wendy Robins <wendy@therobins.net>
2022-01-27 14:37:42 -0500

    added error handling

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -22,26 +22,14 @@ public class Main {
         carDealerWendy.getCurrentCarsAvailable().add(car2);
         carDealerWendy.getCurrentCarsAvailable().add(car3);
 
-        // call the displayInventory method
+        // call the displayInventory method from the Dealership class
         carDealerWendy.displayInventory();
 
         System.out.println("\r\n ====== Welcome to Car Dealer Wendy ======== \r\n");
-//        System.out.println("Enter: \n (1) to reserve a car \n (2) to return a car \n (3) to exit the dealership");
-//
-//        int custInput = 0;
-//        if (!scanner.hasNextInt() ) {
-//            System.out.println("Invalid entry.  Please try again or exit the program.");
-//            //scanner.nextLine();  // Consume newline left-over
-//        }
-//
-//
-//        custInput = scanner.nextInt();
-//        scanner.nextLine();  // Consume newline left-over
 
-        String custCarMakeToReserve;
-        String custCarMakeToReturn;
-        int custInput = 0;
-//        while(custInput <= 3 && custInput > 0) {     //execute the code if this is true
+        String customerCarMakeToReserve;
+        String customerCarMakeToReturn;
+        int customerInput = 0;
         while(true) {
 
             System.out.println("Enter: \n (1) to reserve a car \n (2) to return a car \n (3) to exit the dealership");
@@ -51,25 +39,26 @@ public class Main {
                 scanner.nextLine();  // Consume newline left-over
             }
 
-            custInput = scanner.nextInt();
+            customerInput = scanner.nextInt();
             scanner.nextLine();  // Consume newline left-over
 
-            switch (custInput) {
+            switch (customerInput) {
 
                 case 1:  // reserve car
 
                     if (carDealerWendy.getCurrentCarsAvailable().size() == 0) {  // no rental cars available
                         System.out.println("Sorry, but there are no rental cars available at this time.");
                     } else{         // if car rental inventory exists
+                        System.out.println(carDealerWendy.displayInventory());
                         System.out.println("Enter car make to RESERVE");
-                        custCarMakeToReserve = scanner.nextLine();
+                        customerCarMakeToReserve = scanner.nextLine();
 
                         // *** check if carMake is available.
-                        if (carDealerWendy.reserveCar(custCarMakeToReserve)) {
+                        if (carDealerWendy.reserveCar(customerCarMakeToReserve)) {
                             // *** reserve the car
-                            System.out.println("\r\n ====== A " + custCarMakeToReserve + " is now reserved for you. ======== \r\n");
+                            System.out.println("\r\n ====== A " + customerCarMakeToReserve + " is now reserved for you. ======== \r\n");
                         } else {
-                            System.out.println(custCarMakeToReserve + " is not available at this time.\r");
+                            System.out.println(customerCarMakeToReserve + " is not available at this time.\r");
                         }
                     }
                     break;
@@ -80,21 +69,23 @@ public class Main {
                     if (carDealerWendy.getCurrentCarsBooked().size() == 0) {  // if nothing is in reserved list
                         System.out.println("No reserved cars to return at this time");
                     } else {
+                        System.out.println(carDealerWendy.displayInventoryReserved());
                         System.out.println("Enter car make to RETURN");
-                        custCarMakeToReturn = scanner.nextLine();
+                        customerCarMakeToReturn = scanner.nextLine();
 
                         // ** check if carMake is in the reserved list of cars.
-                        if (carDealerWendy.returnCar(custCarMakeToReturn)) {
+                        if (carDealerWendy.returnCar(customerCarMakeToReturn)) {
                             // *** return the car
-                            System.out.println("\r\n ====== A " + custCarMakeToReturn + " has been returned by you. ======== \r\n");
+                            System.out.println("\r\n ====== A " + customerCarMakeToReturn + " has been returned by you. ======== \r\n");
                         } else {
-                            System.out.println(custCarMakeToReturn = " is not in our records. \r");
+                            System.out.println(customerCarMakeToReturn = " is not in our records. \r");
                         }
                     }
                     break;
 
                 case 3:
                     System.out.println("Exiting Car Dealer Wendy Application.  Bye-bye.");
+
                     break;
 
                 default:
@@ -102,13 +93,10 @@ public class Main {
                     break;
             }
 
-//            if (custInput == 3) {   // exit the application
-//                break;
-//            } else {    // prompt customer
-//                System.out.println("Enter: \n (1) to reserve a car \n (2) to return a car \n (3) to exit the dealership");
-//                custInput = scanner.nextInt();
-//                scanner.nextLine();  // Consume newline left-over
-//            }
+            if (customerInput == 3) {   // exit the application
+                break;
+            }
+
         }
 
     }
